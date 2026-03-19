85fd3942f32db30c319d95312ac938717401537f
kauatwn <kaualopescontato@gmail.com>
2026-03-19 16:32:17 -0300

    feat: add IGetDocumentStatusUseCase and implementation for retrieving document status

---
+++ src/DistributedFileProcessor.Application/UseCases/Documents/GetStatus/IGetDocumentStatusUseCase.cs
@@ -0,0 +1,8 @@
+﻿using DistributedFileProcessor.Application.DTOs.Responses;
+
+namespace DistributedFileProcessor.Application.UseCases.Documents.GetStatus;
+
+public interface IGetDocumentStatusUseCase
+{
+    Task<DocumentStatusResponse?> ExecuteAsync(Guid jobId, CancellationToken cancellationToken = default);
+}
