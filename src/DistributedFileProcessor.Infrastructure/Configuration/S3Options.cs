c8bee292c4435078d77cf73422b6296b9b24bb48
kauatwn <kaualopescontato@gmail.com>
2026-03-19 18:35:58 -0300

    feat: add S3Options and SqsOptions classes for AWS configuration

---
+++ src/DistributedFileProcessor.Infrastructure/Configuration/S3Options.cs
@@ -0,0 +1,8 @@
+﻿namespace DistributedFileProcessor.Infrastructure.Configuration;
+
+public sealed class S3Options
+{
+    public const string SectionName = "AWS:S3";
+
+    public string BucketName { get; init; } = string.Empty;
+}
