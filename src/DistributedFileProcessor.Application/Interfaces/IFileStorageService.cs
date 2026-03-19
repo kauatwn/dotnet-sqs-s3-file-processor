73ceb3385854c3d0af3ad7855c31f3e3f7eab930
kauatwn <kaualopescontato@gmail.com>
2026-03-19 16:14:43 -0300

    feat: add IFileStorageService interface for file storage operations

---
+++ src/DistributedFileProcessor.Application/Interfaces/IFileStorageService.cs
@@ -0,0 +1,7 @@
+﻿namespace DistributedFileProcessor.Application.Interfaces;
+
+public interface IFileStorageService
+{
+    Task<string> UploadFileAsync(string fileName, Stream fileStream, CancellationToken cancellationToken = default);
+    Task<Stream> DownloadFileAsync(string s3ObjectKey, CancellationToken cancellationToken = default);
+}
