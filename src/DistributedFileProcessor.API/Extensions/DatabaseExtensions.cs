dd6980fbf9a0fbf53d70fd2df8c86c8ca70d21ee
kauatwn <kaualopescontato@gmail.com>
2026-03-19 20:19:04 -0300

    feat: add database migration extension method for application startup

---
+++ src/DistributedFileProcessor.API/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using DistributedFileProcessor.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DistributedFileProcessor.API.Extensions;
+
+[ExcludeFromCodeCoverage(Justification = "Infrastructure wrapper for database initialization")]
+public static class DatabaseExtensions
+{
+    public static async Task ApplyMigrationsAsync(this WebApplication app)
+    {
+        using IServiceScope scope = app.Services.CreateScope();
+        FileProcessorDbContext context = scope.ServiceProvider.GetRequiredService<FileProcessorDbContext>();
+
+        await context.Database.MigrateAsync();
+    }
+}
