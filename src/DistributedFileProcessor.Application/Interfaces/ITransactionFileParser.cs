5ac85f1e53d37df8069e187468cb0d00c3687cf2
kauatwn <kaualopescontato@gmail.com>
2026-03-19 16:17:22 -0300

    feat: add ITransactionFileParser interface for parsing transaction files

---
+++ src/DistributedFileProcessor.Application/Interfaces/ITransactionFileParser.cs
@@ -0,0 +1,8 @@
+﻿using DistributedFileProcessor.Domain.Entities;
+
+namespace DistributedFileProcessor.Application.Interfaces;
+
+public interface ITransactionFileParser
+{
+    IAsyncEnumerable<TransactionRecord> ParseStreamAsync(Stream fileStream, Guid jobId, CancellationToken cancellationToken = default);
+}
