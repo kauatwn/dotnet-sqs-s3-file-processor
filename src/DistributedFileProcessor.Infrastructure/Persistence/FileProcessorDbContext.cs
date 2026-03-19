19f8298b093ac08de94671fa27abe836369b193f
kauatwn <kaualopescontato@gmail.com>
2026-03-19 18:28:17 -0300

    feat: add FileProcessorDbContext for managing document processing jobs and transaction records

---
+++ src/DistributedFileProcessor.Infrastructure/Persistence/FileProcessorDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DistributedFileProcessor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DistributedFileProcessor.Infrastructure.Persistence;
+
+public sealed class FileProcessorDbContext(DbContextOptions<FileProcessorDbContext> options) : DbContext(options)
+{
+    public DbSet<DocumentProcessJob> DocumentProcessJobs => Set<DocumentProcessJob>();
+    public DbSet<TransactionRecord> TransactionRecords => Set<TransactionRecord>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FileProcessorDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
