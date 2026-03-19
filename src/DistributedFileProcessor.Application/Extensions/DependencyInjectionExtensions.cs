4d5e6299981366722753485044fc19837689dd1b
kauatwn <kaualopescontato@gmail.com>
2026-03-19 16:33:00 -0300

    feat: add DependencyInjectionExtensions for application service registration

---
+++ src/DistributedFileProcessor.Application/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using DistributedFileProcessor.Application.UseCases.Documents.GetStatus;
+using DistributedFileProcessor.Application.UseCases.Documents.Process;
+using DistributedFileProcessor.Application.UseCases.Documents.Upload;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DistributedFileProcessor.Application.Extensions;
+
+[ExcludeFromCodeCoverage(Justification = "Pure dependency injection configuration")]
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IUploadDocumentUseCase, UploadDocumentUseCase>();
+        services.AddScoped<IProcessDocumentUseCase, ProcessDocumentUseCase>();
+        services.AddScoped<IGetDocumentStatusUseCase, GetDocumentStatusUseCase>();
+
+        return services;
+    }
+}
