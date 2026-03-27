58b1fd5b5359f5264c29d1a61796fe92a0c06620
kauatwn <kaualopescontato@gmail.com>
2026-03-27 20:10:03 -0300

    chore: improve Serilog configuration by conditionally adding Seq logging

--- src/DistributedFileProcessor.API/Program.cs
+++ src/DistributedFileProcessor.API/Program.cs
@@ -5,12 +5,20 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSerilog((services, loggerConfiguration) => loggerConfiguration
-    .ReadFrom.Configuration(builder.Configuration)
-    .Enrich.FromLogContext()
-    .Enrich.WithProperty("Application", builder.Environment.ApplicationName)
-    .WriteTo.Console()
-    .WriteTo.Seq(builder.Configuration["Seq:ServerUrl"] ?? "http://localhost:5341"));
+builder.Services.AddSerilog((services, loggerConfiguration) => 
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.FromLogContext()
+        .Enrich.WithProperty("Application", builder.Environment.ApplicationName)
+        .WriteTo.Console();
+
+    string? seqServerUrl = builder.Configuration["Seq:ServerUrl"];
+    if (!string.IsNullOrWhiteSpace(seqServerUrl))
+    {
+        loggerConfiguration.WriteTo.Seq(seqServerUrl);
+    }
+});
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
--- src/DistributedFileProcessor.Worker/Program.cs
+++ src/DistributedFileProcessor.Worker/Program.cs
@@ -5,12 +5,20 @@ using Serilog;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.AddSerilog((services, loggerConfiguration) => loggerConfiguration
-    .ReadFrom.Configuration(builder.Configuration)
-    .Enrich.FromLogContext()
-    .Enrich.WithProperty("Application", builder.Environment.ApplicationName)
-    .WriteTo.Console()
-    .WriteTo.Seq(builder.Configuration["Seq:ServerUrl"] ?? "http://localhost:5341"));
+builder.Services.AddSerilog((services, loggerConfiguration) => 
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.FromLogContext()
+        .Enrich.WithProperty("Application", builder.Environment.ApplicationName)
+        .WriteTo.Console();
+
+    string? seqServerUrl = builder.Configuration["Seq:ServerUrl"];
+    if (!string.IsNullOrWhiteSpace(seqServerUrl))
+    {
+        loggerConfiguration.WriteTo.Seq(seqServerUrl);
+    }
+});
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
