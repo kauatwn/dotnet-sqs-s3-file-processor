76774c6fc70cf65d4ba56c195c6a8d8ef790d495
kauatwn <kaualopescontato@gmail.com>
2026-08-06 12:12:12 -0300

    refactor: remove Seq logging configuration and local stack resource initialization in Program.cs

--- src/DistributedFileProcessor.Worker/Program.cs
+++ src/DistributedFileProcessor.Worker/Program.cs
@@ -12,13 +12,6 @@ builder.Services.AddSerilog((_, loggerConfiguration) =>
         .Enrich.FromLogContext()
         .Enrich.WithProperty("Application", builder.Environment.ApplicationName)
         .WriteTo.Console();
-
-    string? seqServerUrl = builder.Configuration["Seq:ServerUrl"];
-    
-    if (!string.IsNullOrWhiteSpace(seqServerUrl))
-    {
-        loggerConfiguration.WriteTo.Seq(seqServerUrl);
-    }
 });
 
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -27,10 +20,5 @@ builder.Services.AddApplication();
 builder.Services.AddHostedService<DocumentProcessingWorker>();
 
 IHost host = builder.Build();
-
-if (host.Services.GetRequiredService<IHostEnvironment>().IsDevelopment())
-{
-    await host.EnsureLocalStackResourcesAsync();
-}
 
 await host.RunAsync();
