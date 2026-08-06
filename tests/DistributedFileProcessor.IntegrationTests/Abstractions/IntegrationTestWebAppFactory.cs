b83d884aa31f1d544f75a71f909b57d714642ac8
kauatwn <kaualopescontato@gmail.com>
2026-08-06 12:22:23 -0300

    refactor: update IntegrationTestWebAppFactory to use LocalStack for AWS services

--- tests/DistributedFileProcessor.IntegrationTests/Abstractions/IntegrationTestWebAppFactory.cs
+++ tests/DistributedFileProcessor.IntegrationTests/Abstractions/IntegrationTestWebAppFactory.cs
@@ -1,4 +1,10 @@
-﻿using DistributedFileProcessor.Worker;
+using System.Text.Json;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using DistributedFileProcessor.Worker;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
@@ -16,19 +22,30 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
         .WithPassword("postgres")
         .Build();
 
-    private readonly LocalStackContainer _localStackContainer = new LocalStackBuilder("localstack/localstack:4.14.0")
+    public LocalStackContainer LocalStack { get; } = new LocalStackBuilder("localstack/localstack:2026.07.0")
+        .WithEnvironment("LOCALSTACK_AUTH_TOKEN", Environment.GetEnvironmentVariable("LOCALSTACK_AUTH_TOKEN"))
         .Build();
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        Environment.SetEnvironmentVariable("AWS_ACCESS_KEY_ID", "test");
+        Environment.SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", "test");
+        Environment.SetEnvironmentVariable("AWS_REGION", "us-east-1");
+
         builder.ConfigureAppConfiguration((_, config) =>
         {
-            string localStackUrl = _localStackContainer.GetConnectionString();
+            string localStackUrl = LocalStack.GetConnectionString();
 
             config.AddInMemoryCollection(new Dictionary<string, string?>
             {
                 ["ConnectionStrings:DefaultConnection"] = _dbContainer.GetConnectionString(),
-                ["LocalStack:ServiceUrl"] = localStackUrl,
+                ["AWS:AccessKey"] = "test",
+                ["AWS:SecretKey"] = "test",
+                ["AWS:ServiceURL"] = localStackUrl,
+                ["AWS:SQS:ServiceURL"] = localStackUrl,
+                ["AWS:S3:ServiceURL"] = localStackUrl,
+                ["AWS:S3:ForcePathStyle"] = "true",
+                ["AWS:Region"] = "us-east-1",
 
                 ["AWS:SQS:QueueUrl"] = $"{localStackUrl}/000000000000/integration-test-queue",
                 ["AWS:SQS:QueueName"] = "integration-test-queue",
@@ -41,11 +58,93 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
         builder.ConfigureServices(services =>
         {
             services.AddHostedService<DocumentProcessingWorker>();
+
+            string localStackUrl = LocalStack.GetConnectionString();
+            BasicAWSCredentials credentials = new("test", "test");
+
+            services.AddSingleton<IAmazonS3>(_ => new AmazonS3Client(credentials, new AmazonS3Config
+            {
+                ServiceURL = localStackUrl,
+                AuthenticationRegion = "us-east-1",
+                ForcePathStyle = true,
+                UseHttp = true
+            }));
+
+            services.AddSingleton<IAmazonSQS>(_ => new AmazonSQSClient(credentials, new AmazonSQSConfig
+            {
+                ServiceURL = localStackUrl,
+                AuthenticationRegion = "us-east-1",
+                UseHttp = true
+            }));
         });
     }
 
     public async ValueTask InitializeAsync()
     {
-        await Task.WhenAll(_dbContainer.StartAsync(), _localStackContainer.StartAsync());
+        await Task.WhenAll(_dbContainer.StartAsync(), LocalStack.StartAsync());
+        await SetupLocalStackResourcesAsync();
+    }
+
+    private async Task SetupLocalStackResourcesAsync()
+    {
+        string localStackUrl = LocalStack.GetConnectionString();
+        BasicAWSCredentials credentials = new("test", "test");
+
+        using AmazonS3Client s3Client = new(credentials, new AmazonS3Config
+        {
+            ServiceURL = localStackUrl,
+            AuthenticationRegion = "us-east-1",
+            ForcePathStyle = true,
+            UseHttp = true
+        });
+
+        using AmazonSQSClient sqsClient = new(credentials, new AmazonSQSConfig
+        {
+            ServiceURL = localStackUrl,
+            AuthenticationRegion = "us-east-1",
+            UseHttp = true
+        });
+
+        const string bucketName = "integration-test-bucket";
+        const string mainQueueName = "integration-test-queue";
+        const string dlqName = "integration-test-dlq";
+
+        await s3Client.PutBucketAsync(bucketName);
+
+        CreateQueueResponse dlqResponse = await sqsClient.CreateQueueAsync(dlqName);
+        GetQueueAttributesResponse dlqAttr = await sqsClient.GetQueueAttributesAsync(dlqResponse.QueueUrl, ["QueueArn"]);
+        string dlqArn = dlqAttr.QueueARN;
+
+        string redrivePolicy = JsonSerializer.Serialize(new
+        {
+            deadLetterTargetArn = dlqArn,
+            maxReceiveCount = "3"
+        });
+
+        CreateQueueResponse mainQueueResponse = await sqsClient.CreateQueueAsync(new CreateQueueRequest
+        {
+            QueueName = mainQueueName,
+            Attributes = new Dictionary<string, string>
+            {
+                { "RedrivePolicy", redrivePolicy },
+                { "VisibilityTimeout", "60" }
+            }
+        });
+
+        GetQueueAttributesResponse mainAttr = await sqsClient.GetQueueAttributesAsync(mainQueueResponse.QueueUrl, ["QueueArn"]);
+        string mainQueueArn = mainAttr.QueueARN;
+
+        await s3Client.PutBucketNotificationAsync(new PutBucketNotificationRequest
+        {
+            BucketName = bucketName,
+            QueueConfigurations =
+            [
+                new QueueConfiguration
+                {
+                    Events = [EventType.ObjectCreatedAll],
+                    Queue = mainQueueArn
+                }
+            ]
+        });
     }
 }
