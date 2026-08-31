d75e452d8715310d9c6073cc54ade9e944a9b02d
kauatwn <kaualopescontato@gmail.com>
2026-08-31 15:40:58 -0300

    refactor: streamline argument validation in DocumentProcessJob constructor

--- src/DistributedFileProcessor.Domain/Entities/DocumentProcessJob.cs
+++ src/DistributedFileProcessor.Domain/Entities/DocumentProcessJob.cs
@@ -17,15 +17,8 @@ public sealed class DocumentProcessJob
 
     public DocumentProcessJob(string fileName, string s3ObjectKey)
     {
-        if (string.IsNullOrWhiteSpace(fileName))
-        {
-            throw new ArgumentException("File name must not be empty.", nameof(fileName));
-        }
-
-        if (string.IsNullOrWhiteSpace(s3ObjectKey))
-        {
-            throw new ArgumentException("S3 object key must not be empty.", nameof(s3ObjectKey));
-        }
+        ArgumentException.ThrowIfNullOrWhiteSpace(fileName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(s3ObjectKey);
 
         Id = Guid.NewGuid();
         FileName = fileName;
@@ -36,20 +29,9 @@ public sealed class DocumentProcessJob
 
     public DocumentProcessJob(Guid id, string fileName, string s3ObjectKey)
     {
-        if (id == Guid.Empty)
-        {
-            throw new ArgumentException("Job ID must not be empty.", nameof(id));
-        }
-
-        if (string.IsNullOrWhiteSpace(fileName))
-        {
-            throw new ArgumentException("File name must not be empty.", nameof(fileName));
-        }
-
-        if (string.IsNullOrWhiteSpace(s3ObjectKey))
-        {
-            throw new ArgumentException("S3 object key must not be empty.", nameof(s3ObjectKey));
-        }
+        ArgumentOutOfRangeException.ThrowIfEqual(id, Guid.Empty);
+        ArgumentException.ThrowIfNullOrWhiteSpace(fileName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(s3ObjectKey);
 
         Id = id;
         FileName = fileName;
